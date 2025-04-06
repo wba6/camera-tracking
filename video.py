0fbd9e5519b87e531a0a055f82dffc4c7ae97ada
wba6 <bradenaey1@gmail.com>
2025-04-06 14:09:25 -0400

    Add flask server

--- video.py
+++ video.py
@@ -1,16 +1,12 @@
 import cv2
+from flask import Flask, Response
 import numpy as np
 from adafruit_servokit import ServoKit
 
 kit = ServoKit(channels=16)
-
-# servo on channel zero
 servo_channel = 0
-
-#set servo pulse width range 
 kit.servo[servo_channel].set_pulse_width_range(500,2500)
 
-# function to set the angle of the servo
 def set_servo_angle(angle, channel=0):
     if 0 <= angle <= 180:
         kit.servo[channel].angle = angle
@@ -19,61 +15,56 @@ def set_servo_angle(angle, channel=0):
         print("Angle must be between 0 and 180 degrees.")
 
 cap = cv2.VideoCapture(0)
-
-#defualt x_medium
-_, frame = cap.read()
-rows,cols, _, = frame.shape
-x_medium = int(cols/2)
-
-# set center of the screen
-center = int(cols/2)
-
-#degrees
 position = 90
-
-# pixels
 deadzone = 40
-
-# degrees
 servoMovementSpeed = 1.5
 
-# defualt angle 
 set_servo_angle(90)
-while True:
+
+app = Flask(__name__)
+
+def gen_frames():
+    global position
     _, frame = cap.read()
-    
-    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    rows, cols, _ = frame.shape
+    center = cols // 2
+    x_medium = center
 
-    #red color 
-    low_red = np.array([161,155,84])
-    high_red = np.array([179,255,255])
-    red_mask = cv2.inRange(hsv_frame, low_red, high_red)
+    while True:
+        success, frame = cap.read()
+        if not success:
+            continue
 
-    # get the red objects
-    contours, _ = cv2.findContours(red_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        low_red = np.array([161,155,84])
+        high_red = np.array([179,255,255])
+        red_mask = cv2.inRange(hsv_frame, low_red, high_red)
 
-    # sort contours so biggest is first
-    contours = sorted(contours, key=lambda x:cv2.contourArea(x), reverse=True)
+        contours, _ = cv2.findContours(red_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        contours = sorted(contours, key=lambda x:cv2.contourArea(x), reverse=True)
 
+        for cnt in contours:
+            (x,y,w,h) = cv2.boundingRect(cnt)
+            cv2.rectangle(frame, (x,y),(x + w, y+h), (0,255,0),2)
+            x_medium = int((x + x + w) / 2)
+            break
 
-    for cnt in contours:
-        (x,y,w,h) = cv2.boundingRect(cnt)
+        cv2.line(frame, (x_medium,0), (x_medium, rows), (0,255,0),2)
 
-        cv2.rectangle(frame, (x,y),(x + w, y+h), (0,255,0),2)
+        if x_medium < center - deadzone:
+            position += servoMovementSpeed
+        elif x_medium > center + deadzone:
+            position -= servoMovementSpeed
+        set_servo_angle(position)
 
-        # find the center line of the biggest object
-        x_medium = int((x + x + w) / 2)
-        break
+        ret, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-    cv2.line(frame, (x_medium,0), (x_medium, 480), (0,255,0),2)
-    cv2.imshow("Frame", frame)
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-    # move servo left or right to follow object
-    if x_medium < center - deadzone:
-        position += servoMovementSpeed
-    elif x_medium > center + deadzone:
-        position -= servoMovementSpeed
-    set_servo_angle(position)
-
-cap.release()
-c2.destroyAllWindows()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=False, port=5000)
